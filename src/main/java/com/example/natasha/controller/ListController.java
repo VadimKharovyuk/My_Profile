2ba51b1d67734cf6a1a6f786f0259a345ac87d8c
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-18 08:32:31 +0530

    редирект с таблицы

--- src/main/java/com/example/natasha/controller/ListController.java
+++ src/main/java/com/example/natasha/controller/ListController.java
@@ -1,41 +1,40 @@
-package com.example.natasha.controller;
+    package com.example.natasha.controller;
 
-import com.example.natasha.model.Customer;
-import com.example.natasha.repozitory.ServiceCustomer;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+    import com.example.natasha.model.Customer;
+    import com.example.natasha.repozitory.ServiceCustomer;
+    import lombok.AllArgsConstructor;
+    import org.springframework.stereotype.Controller;
+    import org.springframework.ui.Model;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
+    import java.util.List;
 
-@Controller
-@AllArgsConstructor
-public class ListController {
-    private final ServiceCustomer  serviceCustomer;
- @GetMapping("/welcome/new-user/api/all")
+    @Controller
+    @AllArgsConstructor
+    public class ListController {
+        private final ServiceCustomer  serviceCustomer;
+     @GetMapping("/welcome/new-user/api/all")
+     public String list ( Model model){
+            List<Customer> customerList = serviceCustomer.findAll();
+            model.addAttribute("customerList",customerList);
 
-    public String list ( Model model){
-        List<Customer> customerList = serviceCustomer.findAll();
-        model.addAttribute("customerList",customerList);
+            return "table-list";
+        }
+        @GetMapping("/welcome/new-user/api/all/{id}")
+        public String deletebyId(@PathVariable Long id){
+         serviceCustomer.deleteById(id);
+         return  "redirect:/welcome";
 
-        return "table-list";
+        }
+
+
+        @GetMapping("/registration")
+        public String registration() {
+            return "registration";
+        }
+
+
+
+
     }
-    @GetMapping("/welcome/new-user/api/all/{id}")
-    public String deletebyId(@PathVariable Long id){
-     serviceCustomer.deleteById(id);
-     return   "redirect:/welcome";
-
-    }
-
-
-    @GetMapping("/registration")
-    public String registration() {
-        return "registration";
-    }
-
-
-
-    
-}
